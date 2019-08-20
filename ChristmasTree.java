46833b487bf2460fa19def0e7d718a96b9fd7b2f
Caleb Yang <caleb.yanghannchih93@gmail.com>
2019-08-20 10:40:43 +0800

    Christmas Tree.java code uploaded

---
+++ ChristmasTree.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+
+public class ChristmasTree {
+
+    public static void main(String[] args) {
+        Scanner Height = new Scanner(System.in);
+        System.out.println("Enter Height of Christmas Tree: :");
+
+        int num = Height.nextInt();
+        Height.close();
+
+        System.out.println("The number entered by user: " + num);
+
+        Scanner SegmentHeight = new Scanner(System.in);
+        System.out.println("Enter Segment Height of Christmas Tree: :");
+
+        int num2 = SegmentHeight.nextInt();
+        SegmentHeight.close();
+
+        Point[] widthHeight = new Point['/' + '\\'];
+        // Point [] slopeofCT = new Point['/'] + 2new Point [isWhiteSpace() ] +
+        // for ( num2++;) {
+        System.out.println("/\\" + "\n" + "/\\");
+
+        Point[] segmentHeight = new Point['/' + '\n' + '\\' + '\\'];
+        Point[] WidthHeight1 = new Point['/' + '\n' + '\\'];
+        Point[] row = new Point['/' + '\n' + '\n' + '\\'];
+
+        Scanner WidthHeight = new Scanner(System.in);
+        System.out.println("Enter Segment Width of Christmas Tree: :");
+
+        int num3 = WidthHeight.nextInt();
+        WidthHeight.close();
+        System.out.println("");
+
+    }
+}
+// Scanner Slope = new Scanner(System.in);
+// System.out.println("Enter slope of Christmas Tree: :");
+// Scanner Base = new Scanner(System.in);
+// System.out.println("Enter base of Christmas Tree: :");
+// System.out.println("");
+// int i,j,k,n;
+// n = nextint();
+
+// for (i=1; i< n; i++) {
+// for(j=n+(n/2); j>i; j--) {
+// System.out.println("/\\"); }
+// for (k=1; k<=2*i-1; k++) {
+// System.out.print("--");
+// }
+// System.out.println("");
+// }
+// for (i =1; i <n+(n/2); i++) {
+// for (j=n+(n/2); j>i; j--) {
+// System.out.print( "/\\ ");
+// }
+// for (k=1; k<=2*i-1; k++) {
+// System.out.print("--");
+// }
+// System.out.println("");
+// }
+// }
+// }
